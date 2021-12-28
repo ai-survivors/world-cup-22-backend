4b3af38f59350a023c0a1ce703148ef039e15c29
tradalhariri <trad.alhariri@gmail.com>
2021-12-28 18:49:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# World Cup 2022 Qatar 
---
+++ worldcup22/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Worldcup22Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'worldcup22'
